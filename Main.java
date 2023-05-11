9d439f98b5a274f3073bc38fbf3866f4191e1bb9
Daniel Ledezma <Daniel.ledezmadon@gmail.com>
2023-05-11 11:49:50 -0600

    Version 1.0

--- Main.java
+++ Main.java
@@ -1,6 +1,15 @@
 class Main {
 
     public static void main(String[] args) {
-
+    
+    	int a = Integer.parseInt(args[0]);
+    	int b = Integer.parseInt(args[1]);
+    	
+	SumaDosNumeros obj = new SumaDosNumeros(); //new: genera un espacio en memoria que va a contener la estructura de la clase
+	
+	System.out.println(obj.suma(a, b));
+	
     }
 }
+
+
